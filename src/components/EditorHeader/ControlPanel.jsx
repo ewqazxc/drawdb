48e0c74161d90416a8613f67ddf18d19ab4433c9
Kratik <kratikbohra5@gmail.com>
2025-03-17 03:20:59 +0530

    Toggle datatypes visibility (#366)
    * showDatatype setting added in view control panel
    
    * removed shortcut
    
    can be discussed later
    
    * fixed linting issue and made sure the condition check is above div element
    
    in table.jsx code
    in the else part im directly returning the element by keeping a check  for settings.showDataTypes?
    which helps to not add empty element
    
    for setSetting issue
    I used the similar code used just above the snippet and modified it

--- src/components/EditorHeader/ControlPanel.jsx
+++ src/components/EditorHeader/ControlPanel.jsx
@@ -1315,6 +1315,15 @@ export default function ControlPanel({
         function: resetView,
         shortcut: "Ctrl+R",
       },
+      show_datatype: {
+        state: settings.showDataTypes ? (
+          <i className="bi bi-toggle-on" />
+        ) : (
+          <i className="bi bi-toggle-off" />
+        ),
+        function: () =>
+          setSettings((prev) => ({ ...prev, showDataTypes: !prev.showDataTypes })),
+      },
       show_grid: {
         state: settings.showGrid ? (
           <i className="bi bi-toggle-on" />
