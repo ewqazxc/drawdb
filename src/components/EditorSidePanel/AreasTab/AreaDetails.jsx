69af6c3fe648f8e48f409a40282386df5aaf7730
XC <603550500@qq.com>
2025-03-21 09:48:10 +0800

    Merge tailwind v4

--- src/components/EditorSidePanel/AreasTab/AreaDetails.jsx
+++ src/components/EditorSidePanel/AreasTab/AreaDetails.jsx
@@ -28,7 +28,7 @@ export default function AreaInfo({ data, i }) {
       id={`scroll_area_${data.id}`}
       className="my-3"
     >
-      <Col span={18} className="flex items-center">
+      <Col span={18} className="!flex items-center">
         <LocateTargetPosition position={{ x: data.x, y: data.y }} />
         <Input
           value={data.name}
