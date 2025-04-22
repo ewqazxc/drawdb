d92ec5e57181f884a6a4465cc0bde2a4eda8002f
Jean Franz <49456614+JeanFranz73@users.noreply.github.com>
2025-04-22 17:59:26 -0300

    Add missing pt-BR translations (#420)

--- src/i18n/locales/zh.js
+++ src/i18n/locales/zh.js
@@ -48,7 +48,6 @@ const zh = {
     reset_view: "重置视图",
     show_datatype: "显示数据类型",
     show_grid: "显示网格",
-    show_datatype: "显示数据类型",
     show_cardinality: "显示关系",
     theme: "主题",
     light: "浅色",
