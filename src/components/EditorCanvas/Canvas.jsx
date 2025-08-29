ae36ea4baf9f19725c713703aaee4a2b40df93b3
1ilit <1ilit@proton.me>
2025-08-29 21:32:50 +0400

    Set min area size (#567)

--- src/components/EditorCanvas/Canvas.jsx
+++ src/components/EditorCanvas/Canvas.jsx
@@ -7,6 +7,7 @@ import {
   ObjectType,
   gridSize,
   gridCircleRadius,
+  minAreaSize,
 } from "../../data/constants";
 import { Toast } from "@douyinfe/semi-ui";
 import Table from "./Table";
@@ -376,6 +377,8 @@ export default function Canvas() {
           newDims.height = y - areaInitDimensions.y;
           break;
       }
+
+      if(newDims.width < minAreaSize || newDims.height < minAreaSize) return;
 
       updateArea(areaResize.id, { ...newDims });
       return;
