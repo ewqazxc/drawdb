b83ffe41b4da5b4826369bef06624f432f3a1a39
1ilit <1ilit@proton.me>
2026-02-05 21:31:55 +0400

    Show table comments on the diagram (#860)

--- src/components/EditorHeader/ControlPanel.jsx
+++ src/components/EditorHeader/ControlPanel.jsx
@@ -543,7 +543,7 @@ export default function ControlPanel({
       minMaxXY.minX = Math.min(minMaxXY.minX, table.x);
       minMaxXY.minY = Math.min(minMaxXY.minY, table.y);
       minMaxXY.maxX = Math.max(minMaxXY.maxX, table.x + settings.tableWidth);
-      minMaxXY.maxY = Math.max(minMaxXY.maxY, table.y + getTableHeight(table));
+      minMaxXY.maxY = Math.max(minMaxXY.maxY, table.y + getTableHeight(table, settings.tableWidth, settings.showComments));
     });
 
     areas.forEach((area) => {
@@ -1517,6 +1517,18 @@ export default function ControlPanel({
       reset_view: {
         function: resetView,
         shortcut: "Enter/Return",
+      },
+      show_comments: {
+        state: settings.showComments ? (
+          <i className="bi bi-toggle-on" />
+        ) : (
+          <i className="bi bi-toggle-off" />
+        ),
+        function: () =>
+          setSettings((prev) => ({
+            ...prev,
+            showComments: !prev.showComments,
+          })),
       },
       show_datatype: {
         state: settings.showDataTypes ? (
